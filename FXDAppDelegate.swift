d4a8b1f8702faceb0832b96ac9c1e31844761288
petershine <petershine@fxceed.com>
2016-05-12 19:29:42 -0400

    IMPLEMENTED dynamic nature

--- FXDAppDelegate.swift
+++ FXDAppDelegate.swift
@@ -78,10 +78,8 @@ class FXDAppDelegate: UIResponder, UIApplicationDelegate {
 	lazy var managedObjectModel: NSManagedObjectModel = {	FXDLog_Func()
 		// The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
 
-		let modelName: String = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as! String
-		FXDLog(modelName)
-
-		let modelURL = NSBundle.mainBundle().URLForResource(modelName, withExtension: "momd")!
+		let bundleName: String = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as! String
+		let modelURL = NSBundle.mainBundle().URLForResource(bundleName, withExtension: "momd")!
 		FXDLog(modelURL)
 		
 		return NSManagedObjectModel(contentsOfURL: modelURL)!
@@ -91,8 +89,16 @@ class FXDAppDelegate: UIResponder, UIApplicationDelegate {
 		// The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
 		// Create the coordinator and store
 
+		var bundleIdentifier: String = NSBundle.mainBundle().infoDictionary?["CFBundleIdentifier"] as! String
+		bundleIdentifier = bundleIdentifier.stringByReplacingOccurrencesOfString(".", withString: "_")
+
+		let sqliteFilename: String = bundleIdentifier + ".sqlite"
+		FXDLog(sqliteFilename)
+
+
+		let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(sqliteFilename)
 		let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-		let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
+
 		var failureReason = "There was an error creating or loading the application's saved data."
 
 		do {
@@ -100,6 +106,7 @@ class FXDAppDelegate: UIResponder, UIApplicationDelegate {
 
 		} catch {
 			// Report any error we got.
+
 			var dict = [String: AnyObject]()
 			dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
 			dict[NSLocalizedFailureReasonErrorKey] = failureReason
@@ -117,9 +124,11 @@ class FXDAppDelegate: UIResponder, UIApplicationDelegate {
 
 	lazy var managedObjectContext: NSManagedObjectContext = {
 		// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+
 		let coordinator = self.persistentStoreCoordinator
 		var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
 		managedObjectContext.persistentStoreCoordinator = coordinator
+
 		return managedObjectContext
 	}()
 
