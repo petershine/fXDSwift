1571ebd99b8b2db190abde6718f394535a1f9fa4
petershine <petershine@fxceed.com>
2016-05-12 20:01:24 -0400

    SEPARATED coredatamodule

--- FXDAppDelegate.swift
+++ FXDAppDelegate.swift
@@ -9,8 +9,13 @@ class FXDAppDelegate: UIResponder, UIApplicationDelegate {
 	//MARK: The app delegate must implement the window property if it wants to use a main storyboard file.
 	var window: UIWindow?
 
+
+	//TODO: Evaluate if I need this all the time
 	var isAppLaunching: Bool?
 	var didFinishLaunching: Bool?
+
+	//MARK: Subclass should initialize according to its requirement
+	var coredataModule: FXDmoduleCoredata?
 
 
 	override init() {	FXDLog_SEPARATE()
@@ -64,89 +69,6 @@ class FXDAppDelegate: UIResponder, UIApplicationDelegate {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 		// Saves changes in the application's managed object context before the application terminates.
 
-		self.saveContext()
-	}
-
-
-	// MARK: - Core Data stack
-	lazy var applicationDocumentsDirectory: NSURL = {
-		// The directory the application uses to store the Core Data store file. This code uses a directory named "{BundleId}" in the application's documents Application Support directory.
-		let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-		return urls[urls.count-1]
-	}()
-
-	lazy var managedObjectModel: NSManagedObjectModel = {	FXDLog_Func()
-		// The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-
-		let bundleName: String = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as! String
-		let modelURL = NSBundle.mainBundle().URLForResource(bundleName, withExtension: "momd")!
-		FXDLog(modelURL)
-		
-		return NSManagedObjectModel(contentsOfURL: modelURL)!
-	}()
-
-	lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {	FXDLog_Func()
-		// The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-		// Create the coordinator and store
-
-		var bundleIdentifier: String = NSBundle.mainBundle().infoDictionary?["CFBundleIdentifier"] as! String
-		bundleIdentifier = bundleIdentifier.stringByReplacingOccurrencesOfString(".", withString: "_")
-
-		let sqliteFilename: String = bundleIdentifier + ".sqlite"
-		FXDLog(sqliteFilename)
-
-
-		let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(sqliteFilename)
-		let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-
-		var failureReason = "There was an error creating or loading the application's saved data."
-
-		do {
-			try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-
-		} catch {
-			// Report any error we got.
-
-			var dict = [String: AnyObject]()
-			dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-			dict[NSLocalizedFailureReasonErrorKey] = failureReason
-
-			dict[NSUnderlyingErrorKey] = error as NSError
-			let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-			// Replace this with code to handle the error appropriately.
-			// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-			NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-			abort()
-		}
-
-		return coordinator
-	}()
-
-	lazy var managedObjectContext: NSManagedObjectContext = {
-		// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-
-		let coordinator = self.persistentStoreCoordinator
-		var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
-		managedObjectContext.persistentStoreCoordinator = coordinator
-
-		return managedObjectContext
-	}()
-
-
-	// MARK: - Core Data Saving support
-	func saveContext () {	FXDLog_Func()
-		FXDLog(managedObjectContext.hasChanges)
-
-		if managedObjectContext.hasChanges {
-			do {
-				try managedObjectContext.save()
-			} catch {
-				// Replace this implementation with code to handle the error appropriately.
-				// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-				let nserror = error as NSError
-				NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-				abort()
-			}
-		}
+		coredataModule?.saveContext()
 	}
 }
