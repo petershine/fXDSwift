9b4e749d680205554e7878c44fe165da2d74e3e2
petershine <petershine@fxceed.com>
2016-12-23 16:03:04 -0500

    BROUGHT old lines

--- FXDmoduleFacebook.swift
+++ FXDmoduleFacebook.swift
@@ -6,6 +6,43 @@
 //  Copyright © 2016 fXceed. All rights reserved.
 //
 
+/* From Old setup
+#ifndef apikeyFacebookAppId
+#define apikeyFacebookAppId	@"000000000000000"
+#endif
+
+#define facebookPermissionBasicInfo	@"basic_info"	//TODO: Obsolete. Update appropriately
+#define facebookPermissionPublishStream	@"publish_stream"
+
+#define	facebookPermissionEmail			@"email"
+#define facebookPermissionPublicProfile	@"public_profile"
+#define facebookPermissionUserFriends	@"user_friends"
+
+#define	facebookPermissionPublishActions	@"publish_actions"
+#define	facebookPermissionManagePages		@"manage_pages"
+
+#define facebookGraphMe				@"me"
+#define facebookGraphMeAccounts		@"me/accounts"
+#define facebookGraphProfileFeed	@"%@/feed"
+#define facebookGraphProfileVideos	@"%@/videos"
+#define facebookGraphAccessToken	@"%@?fields=access_token"
+
+#define urlrootFacebookAPI	@"https://graph.facebook.com/"
+#define urlhostFacebookVideoGraph	@"https://graph-video.facebook.com/"
+
+#define urlstringFacebook(method)	[NSString stringWithFormat:@"%@%@", urlrootFacebookAPI, method]
+#define urlstringFacebookVideoGraph(method)	[NSString stringWithFormat:@"%@%@", urlhostFacebookVideoGraph, method]
+
+#define objkeyFacebookAccessToken	@"access_token"
+#define objkeyFacebookID	@"id"
+#define objkeyFacebookName	@"name"
+#define objkeyFacebookLocale	@"locale"
+#define objkeyFacebookUsername	@"username"
+#define objkeyFacebookCategory	@"category"
+*/
+
+
+
 import UIKit
 import Foundation
 
