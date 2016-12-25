5f5f650e2e3b10402b271ea4a8c2bb062a79af41
petershine <petershine@fxceed.com>
2016-12-25 01:38:38 -0500

    UPDATED

--- FXDmoduleFacebook.swift
+++ FXDmoduleFacebook.swift
@@ -67,31 +67,126 @@ class FXDmoduleFacebook: NSObject {
 		FXDLog(presentingScene)
 		FXDLog(Bool(FBSDKAccessToken.current() != nil))
 
-		// Must be Facebook account
-		guard typeIdentifier == self.typeIdentifier
-			else {
-				callback(false, NSNull())
-				return
-		}
-
 
 		let loginManager: FBSDKLoginManager = FBSDKLoginManager()
 		loginManager.loginBehavior = FBSDKLoginBehavior.systemAccount
 
 
-		let login: FBSDKLoginManager = FBSDKLoginManager()
-
-		login.logIn(withReadPermissions: [facebookPermissionPublicProfile],
+		/*
+		loginManager.logIn(withReadPermissions:
+			["public_profile",
+			 "user_friends",
+			 "email"],
 		            from: presentingScene)
 		{
 			(result:FBSDKLoginManagerLoginResult?,
 			error:Error?) in
 
-			FXDLog(result?.description)
+			callback((result?.isCancelled)! == false, error as Any)
+		}
+*/
 
-			SWIFTLog(result?.isCancelled)
+		//2016-12-24 00:39:05.516130 PopToo[10164:2443443] FBSDKLog: ** WARNING: You are requesting permissions inside the completion block of an existing login.This is unsupported behavior. You should request additional permissions only when they are needed, such as requesting for publish_actionswhen the user performs a sharing action.
+
+
+		loginManager.logIn(withPublishPermissions:["publish_actions",
+		                                           "manage_pages",
+		                                           "publish_pages"],
+		                   from: presentingScene)
+		{ (result:FBSDKLoginManagerLoginResult?,
+			error:Error?) in
+
+
+			FXDLog(result?.description)
+			FXDLog(error)
+
+
+			FXDLog(result?.token.appID)
+			FXDLog(result?.token.expirationDate)
+			FXDLog(result?.token.refreshDate)
+			FXDLog(result?.token.tokenString)
+			FXDLog(result?.token.userID)
+
+			FXDLog(result?.grantedPermissions.description)
+			FXDLog(result?.declinedPermissions.description)
+
+			FXDLog(result?.isCancelled == false)
 
 			callback((result?.isCancelled)! == false, error as Any)
 		}
 	}
+
+	@objc public func showActionSheetFor(typeIdentifier: String, presentingScene: UIViewController, callback: @escaping finishedClosure) {	FXDLog_Func()
+
+		FXDLog(typeIdentifier)
+		FXDLog(presentingScene)
+		FXDLog(Bool(FBSDKAccessToken.current() != nil))
+		FXDLog(self.multiAccountArray)
+
+
+		func PresentActionSheet() {
+			//TODO:
+		}
+
+
+		/*
+		if ((self.multiAccountArray?.count)! > 0) {
+			PresentActionSheet()
+			callback(true, NSNull())
+			return
+		}
+*/
+
+
+		//FBSDKLog: starting with Graph API v2.4, GET requests for /me/accounts should contain an explicit "fields" parameter
+		//https://developers.facebook.com/docs/graph-api/reference/user/
+
+		var collectedAccounts: Array<Any> = []
+
+		let graphRequestMe = FBSDKGraphRequest.init(
+			graphPath: facebookGraphMe,
+			parameters: ["fields": "id, name"])
+
+		let graphRequestAccounts = FBSDKGraphRequest.init(
+			graphPath: facebookGraphMeAccounts,
+			parameters: ["fields": "data"])
+
+		graphRequestMe?.start(completionHandler:
+			{ (requestConnection:FBSDKGraphRequestConnection?,
+				result:Any?,
+				error:Error?) in
+
+
+				FXDLog((result as Any?))
+				FXDLog(error)
+
+				collectedAccounts.append(result as Any)
+				FXDLog(collectedAccounts)
+
+
+				graphRequestAccounts?.start(completionHandler:
+					{ (requestCOnnection:FBSDKGraphRequestConnection?,
+						result:Any?,
+						error:Error?) in
+
+
+						FXDLog((result as Any?))
+						FXDLog(error)
+
+
+						let accounts: Array<Any> = (result as! Dictionary<String, Any>)["data"] as! Array
+						FXDLog(accounts as Any?)
+
+						for account in accounts {
+							collectedAccounts.append(account)
+						}
+						FXDLog(collectedAccounts)
+
+
+						callback(error == nil, collectedAccounts)
+				})
+		})
+	}
+
+
 }
