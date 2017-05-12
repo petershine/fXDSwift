55e1577170a6df8822abcce9d8402a94f935d3bf
petershine <petershine@fxceed.com>
2017-05-12 12:26:27 -0400

    MINIMIZING use of FXDLog()

--- FXDfunctionDeveloper.swift
+++ FXDfunctionDeveloper.swift
@@ -4,42 +4,42 @@ import UIKit
 import Foundation
 
 
+func FXDLog_Func(_ file: NSString = #file, function: NSString = #function) {
+	#if ForDEVELOPER
+		debugPrint(" ")
+		debugPrint("[\(file.lastPathComponent) \(function)]")
+	#endif
+}
+
+
+//TODO: Adopt better debugging
+/*
+func FXDLog_SEPARATE(_ file: NSString = #file, function: NSString = #function) {
+	#if ForDEVELOPER
+		debugPrint("\n\n    [\(file.lastPathComponent) \(function)]")
+	#endif
+}
+
 func FXDLog(_ obj: Any?) {
-#if ForDEVELOPER
-	NSLog("\(String(describing: obj))")
-#endif
+	#if ForDEVELOPER
+		debugPrint("\(String(describing: obj))")
+	#endif
 }
 
 func FXDLog_Observed(_ obj: Any?) {
-#if ForDEVELOPER
-	NSLog("_Observed: \(String(describing: obj))")
-#endif
+	#if ForDEVELOPER
+		debugPrint("_Observed: \(String(describing: obj))")
+	#endif
 }
-
-
-func FXDLog_Func(_ file: NSString = #file, function: NSString = #function) {
-#if ForDEVELOPER
-	NSLog(" ")
-	NSLog("[\(file.lastPathComponent) \(function)]")
-#endif
-}
-
-
-func FXDLog_SEPARATE(_ file: NSString = #file, function: NSString = #function) {
-#if ForDEVELOPER
-	NSLog("\n\n    [\(file.lastPathComponent) \(function)]")
-#endif
-}
-
 
 func FXDLog_OVERRIDE() {
-#if ForDEVELOPER
-	FXDLog("SHOULD OVERRIDE")
-#endif
+	#if ForDEVELOPER
+		FXDLog("SHOULD OVERRIDE")
+	#endif
 }
+*/
 
 
 
 //MARK: Closures
-//typedef void (^FXDcallbackFinish)(SEL caller, BOOL didFinish, id responseObj);
 typealias FXDclosureFinished = (Bool, Any) -> Void
