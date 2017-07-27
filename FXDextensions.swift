18445d39c850b941e429c2fbcab630f49153661f
petershine <petershine@fxceed.com>
2017-07-26 22:08:30 -0400

    MODIFIED to enforce main queue dispatch

--- FXDextensions.swift
+++ FXDextensions.swift
@@ -6,18 +6,28 @@ import Foundation
 
 @objc extension UIAlertController {
 	//FIXME: Re-consider about returning like similar original method
+
 	class func simpleAlert(withTitle title: String?,
-	                        message: String? = nil,
-	                        cancelTitle: String? = NSLocalizedString("OK", comment: ""),
-	                        fromScene: UIViewController? = nil,
-	                        handler: ((UIAlertAction) -> Swift.Void)? = nil) {
-		
+	                       message: String?) {
+		self.simpleAlert(withTitle: title,
+		                 message: message,
+		                 cancelText: nil,
+		                 fromScene: nil,
+		                 handler: nil)
+	}
+
+	class func simpleAlert(withTitle title: String?,
+	                       message: String? = nil,
+	                       cancelText: String? = NSLocalizedString("OK", comment: ""),
+	                       fromScene: UIViewController? = nil,
+	                       handler: ((UIAlertAction) -> Swift.Void)? = nil) {
 		//MARK: Assume this is the condition for simple alerting without choice
+
 		let alert = UIAlertController(title: title,
 		                              message: message,
 		                              preferredStyle: .alert)
 		
-		let cancelAction = UIAlertAction(title: cancelTitle,
+		let cancelAction = UIAlertAction(title: cancelText,
 		                                 style: .cancel,
 		                                 handler: handler)
 		
@@ -30,10 +40,13 @@ import Foundation
 			let mainWindow: UIWindow = UIApplication.shared.windows.last!
 			presentingScene = mainWindow.rootViewController!
 		}
-		
-		presentingScene?.present(alert,
-		                         animated: true,
-		                         completion: nil)
+
+		DispatchQueue.main.async {
+			//Forced dispatch main
+			presentingScene?.present(alert,
+			                         animated: true,
+			                         completion: nil)
+		}
 	}
 }
 
